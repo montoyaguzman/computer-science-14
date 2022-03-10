af614e4ed6051a80470e402820576c98f779b2f5
j.montoya <j.montoya@bbva.com>
2022-03-09 19:21:10 -0600

    feat: ejercicio de matrices

---
+++ 1_Logica/e6-calculadora-matrices/src/script.js
@@ -0,0 +1,20 @@
+// arreglo
+var array = [1, 3, 5, 7];
+
+// arreglo de arreglos => matriz
+var matriz = [
+    [1, 2, 3, 4],
+    [5, 6, 7, 8],
+    [55, 16, 2127, 118],
+    [15, 26, 77, 8],
+    [85, 666, 17, 98],
+];
+
+console.log('array', array);
+console.log('numero 3: ', array[1]);
+console.log('numero 6', matriz);
+// console.log('numero 26', matriz[?][?]);
+// console.log('numero 17', matriz);
+// console.log('numero 2127', matriz);
+// console.log('numero 85', matriz);
+console.log('matriz', matriz[1][1]);
