dd8cb951b75a340e84b0adfb35c59da34add7ede
j.montoya <j.montoya@bbva.com>
2022-03-10 21:09:55 -0600

    feat: arrow functions

--- 2_ECMA/es6/tema3.js
+++ 2_ECMA/es6/tema3.js
@@ -3,20 +3,23 @@ const suma = (numero1, numero2) => {
     return resultado;
 }
 
+// function suma(numero1, numero2) {
+//     let resultado = numero1 + numero2;
+//     return resultado;
+// }
+
 const resta = (numero1, numero2) => {
     let resultado = numero1 - numero2;
     return resultado;
 }
 
-const multiplica = (numero1, numero2) => {
-    let resultado = numero1 * numero2;
-    return resultado;
-}
+// short form
+const multiplica = (numero1, numero2) => numero1 * numero2;
 
-const divide = (numero1, numero2) => {
-    let resultado = numero1 / numero2;
-    return resultado; 
-}
+// short form
+const divide = (numero1, numero2) => numero1 / numero2;
+
+const showMessage = msg => `Error ${msg}`; 
 
 const calculadora = (numero1, numero2, operando) => {
   let resultado = 0;
@@ -34,7 +37,8 @@ const calculadora = (numero1, numero2, operando) => {
       resultado = divide(numero1, numero2);
     break;
     default:
-        console.log('ingresaste algo incorrecto');
+        const mensaje = showMessage('caso no encontrado');
+        console.log(mensaje);
     break;
    } 
    return resultado;
