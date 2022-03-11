3660284cd55688124bc9a781bb068514ac9e21e8
j.montoya <j.montoya@bbva.com>
2022-03-10 19:57:53 -0600

    feat: 1. var, let y const

--- 2_ECMA/es6/index.js
+++ 2_ECMA/es6/index.js
@@ -1,12 +1,33 @@
-
 const PI = 3.1416;
-var numero1 = 1;
-var numero3 = 9;
+const STUDENT_NAME = 'Juan Lopez';
+
+let numero1 = 1111;
+var numero2 = 2222;
+var numero3 = 3333;
 
 function myFunction() {
-  if(true) {
-    numero3 = 90;
+  if (true) {
+    let numero4 = 4444;
+    console.log('dato: ', numero1);
+    console.log('dato: ', numero2);
+    console.log('dato: ', numero3);
+    console.log('dato: ', numero4);
   }
+  console.log('dato: ', numero1);
+  console.log('dato: ', numero2);
+  console.log('dato: ', numero3);
+  // console.log('dato: ', numero4); // error 'ReferenceError: numero4 is not defined' ya que let es una definicion en el bloque
 }
-console.log('Hola', numero3);
+// console.log('dato: ', numero2);
+// PI = 9000; // error 'Assignment to constant variable' ya que a una constante no se le reasignan valores
+
 myFunction();
+
+
+// cual es la diferencia entre declarar e inicializar una variable?
+let myVariable; // declarar
+myVariable = 'otro valor' // inicializar
+let myVariable2 = 'un valor'; // declarar e inicializar
+
+// var otroNumero = 10;
+// console.log(otroNumero);
