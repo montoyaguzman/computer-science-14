a919bacdf637887429de38da9ca711142d68530f
j.montoya <j.montoya@bbva.com>
2022-03-14 18:53:45 -0600

    feat: foreach vs map

--- 2_ECMA/es6/tema4.js
+++ 2_ECMA/es6/tema4.js
@@ -1,11 +1,25 @@
 const arr = [19, 29, 39, 49, { id: 1, name: 'jose'} ];
 
-for(let i = 0; i < arr.length; i++) {
-  console.log('for: ', arr[i]);
-}
+// for(let i = 0; i < arr.length; i++) {
+//   console.log('for: ', arr[i]);
+// }
+
+// const arrowFn = (element, index) => console.log(`patito${index}`, element)
+// arr.map(arrowFn);
 
 // map
-arr.map((element, index) => console.log(`patito${index}`, element)); // arr[i]
+console.log('-- MAP --')
+// arr.map((element, index) => console.log(`patito${index}`, element)); // arr[i]
+const newArr1 = arr.map((element, index) => {
+  console.log(`patito${index}`, element)
+  return element;
+});
+console.log('newArr1: ', newArr1);
 
-const arrowFn = (element, index) => console.log(`patito${index}`, element)
-arr.map(arrowFn);
+// foreach
+console.log('-- FOREACH --')
+// arr.forEach((element, index) => console.log(`patito${index}`, element)); // arr[i]
+const newArr2 = arr.forEach((element, index) => {
+  console.log(`patito${index}`, element)
+});
+console.log('newArr2: ', newArr2);
