65fcb84c772e82bd19ed4e0f3b494025965c926f
oicrruf <vm.reyesal@gmail.com>
2022-03-16 21:27:37 -0600

    feat: icon on lists

--- 3_TODO/assets/js/main.js
+++ 3_TODO/assets/js/main.js
@@ -22,29 +22,60 @@ const languageComplete = myLanguages.filter(
 );
 const languagePending = myLanguages.length - languageComplete.length;
 
-const renderElementList = ({ name }) => {
-  let language = document.createElement("li");
-  language.innerText = name;
-  language.classList.add(
+
+
+
+
+
+const renderElementList = ({ name, complete, start }) => {
+  // Crear elementos necesario
+  let listElement = document.createElement("li");
+  let iconBox = document.createElement("div");
+  let iconPlay = document.createElement("i");
+  let iconPause = document.createElement("i");
+  let iconCheck = document.createElement("i");
+  // Asignamos las clases correspondientes a los iconos
+  iconPlay.classList.add("bi", "bi-play-circle-fill", "text-primary");
+  iconCheck.classList.add("bi", "bi-check-circle-fill", "text-success");
+  iconPause.classList.add("bi", "bi-pause-circle-fill", "text-warning");
+
+  listElement.innerText = name;
+  listElement.classList.add(
     "list-group-item",
     "d-flex",
     "justify-content-between"
   );
-  listLanguagesElement.appendChild(language);
+  // Insertar li en ul
+  listLanguagesElement.appendChild(listElement);
+  // Insertar div en li
+  listElement.appendChild(iconBox);
+  // Insertar i en div
+  if (complete) {
+    iconBox.appendChild(iconCheck);
+  } else if (start && !complete) { // start && complete === false
+    iconBox.appendChild(iconPlay);
+  } else {
+    iconBox.appendChild(iconPause);
+  }
 };
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 myLanguages.forEach(renderElementList);
 languagePendingElement.innerText = languagePending;
 languageCompleteElement.innerText = languageComplete.length;
 languageAllElement.innerText = myLanguages.length;
-
-// `<li class="list-group-item d-flex justify-content-between">
-//   HTML
-//   <div>
-//     <i class="bi bi-trash-fill text-danger"></i>
-//     <i class="bi bi-check-circle-fill text-success"></i>
-//   </div>
-// </li>`
 
 // Diferentes manera de escribir condicionales
 // const aprendido = false
