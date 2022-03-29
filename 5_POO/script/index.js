eb8bce82c185c5403fb87c8b7495dfa7f3009ef9
j.montoya <j.montoya@bbva.com>
2022-03-29 08:52:20 -0600

    feat: diseño de clases y primeros objetos

---
+++ 5_POO/script/index.js
@@ -0,0 +1,34 @@
+// const pokemon = {
+    
+//     name: 'bulbasaur',
+//     type: 'grass',
+//     rankings: { attack: 90, deffense: 80, hp: 100 },
+//     color: 'green',
+//     height: 1,
+//     weight: 5,
+
+//     attack()
+//     pelear()
+//     corre()
+//     defenderse()
+    
+
+// };
+
+// const pokemon2 = {
+//     name: 'ivysaur',
+//     type: 'grass',
+//     rankings: { attack: 95, deffense: 80, hp: 100 },
+//     color: 'green and blue',
+//     height: 1.2,
+//     weight: 55,
+// };
+
+// const pokemon3 = {
+//     name: 'ivysaur',
+//     type: 'grass',
+//     rankings: { attack: 95, deffense: 80, hp: 100 },
+//     color: 'green and blue',
+//     height: 1.2,
+//     weight: 55,
+// };
